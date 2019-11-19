3d0424e53f2debd6530b9c1eadbfb5782657ffb4
JaxonWright <jaxonhwright@gmail.com>
2019-11-18 21:59:19 -0500

    Home: Spotify Now Playing now only shows when I am actively listening to music.
    Tweaks to about card at top to have a description that is visually consistent with rest of site.
    
    Removed unused CSS.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,19 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { LastFmScrobblesComponent } from 'angular-last-fm-scrobbles';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-  lastFmKey : string;
+export class HomeComponent implements OnInit, AfterViewInit {
+  lastFmKey : string; // YOU NEED TO GET THIS YOURSELF
+  lastFmUser : string = 'jxnw'; // PROBABLY SHOULD USE YOUR OWN HERE
+  @ViewChild(LastFmScrobblesComponent,{static: false}) scrobbler : LastFmScrobblesComponent;
+  isCurrentlyPlayingMusic : boolean = false;
 
   constructor(private titleServ : Title) { 
     this.titleServ.setTitle('Jaxon Wright - About');
   }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    setTimeout(()=>{
+      if (this.lastFmKey) {
+        this.scrobbler.isNowPlaying$.subscribe((val)=> {
+          this.isCurrentlyPlayingMusic = val;
+        });
+      }
+    },1000)
   }
 
   getTimeGreeting() : string {
@@ -33,4 +47,6 @@ export class HomeComponent implements OnInit {
     const timeDiff = Math.abs(Date.now() - bday.getTime())
     return Math.floor((timeDiff / (1000 * 3600 * 24))/365.25); 
   }
+
+
 }
