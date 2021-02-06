a3111b031942fc46db33c4401143fc2d2789146c
Scott VandenToorn <vandents@mail.gvsu.edu>
2021-02-05 19:45:12 -0500

    Make correct theme icon show (#23)
    Co-authored-by: vandents <svandentoorn@gmail.com>

--- src/app/elements/navbar/navbar.component.ts
+++ src/app/elements/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AppComponent } from '@app/app.component';
 import { ThemeService, ThemeType } from '@services/theme/theme.service';
@@ -8,28 +8,13 @@ import { ThemeService, ThemeType } from '@services/theme/theme.service';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
-  preventSingle : boolean = false;
-  timer;
+export class NavbarComponent {
   ThemeType = ThemeType;
   
-  constructor(public router : Router, public app : AppComponent, public theme : ThemeService) { }
-
-  ngOnInit() {
-  }
-
-  setTheme(theme : ThemeType) {
-
-    if (theme != ThemeType.Black) {
-      this.timer = setTimeout(()=> {
-        if (!this.preventSingle) this.app.setTheme(theme);
-        else this.preventSingle = false;
-      },200)
-    } else {
-      this.preventSingle = true;
-      clearTimeout(this.timer);
-      this.app.setTheme(theme);
-    }
-  }
+  constructor(
+    public router: Router,
+    public app: AppComponent,
+    public theme: ThemeService
+  ) { }
 
 }
